fafc5eb37273241a3a5b1a8c4411dc35197ae9d0
AKIRAFOX <shakyasarthak2002@gmail.com>
2023-02-15 18:03:33 +0545

    initial 5.0

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (eq,res) =>{
+app.get("/", (req,res) =>{
   res.send("HELLO TESTING  1...2...3...");
 })
 
-app.listen(8000, () =>{
+app.listen(5000, () =>{
   console.log("THIS IS SPY!!!")
 })
