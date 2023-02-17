eea68b4c85bbf1be3285e695b2f682b4b4b3d7cc
AKIRAFOX <122764949+akira-lawliet@users.noreply.github.com>
2023-02-17 17:09:19 +0545

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>"THIS IS SEM PROJECT" - PUNYA</h1>
+     <h1>"THIS IS SEM PROJECT"</h1>
      <h2>NO CSS</h2>
     </div>
   );
