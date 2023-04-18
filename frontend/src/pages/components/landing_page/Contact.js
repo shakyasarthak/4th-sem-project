a5373611dcc462955f803cbc61f1b2589eb4f0c8
AKIRAFOX <122764949+akira-lawliet@users.noreply.github.com>
2023-04-18 16:02:52 +0545

    prasaddhi typo fix

--- frontend/src/pages/components/landing_page/Contact.js
+++ frontend/src/pages/components/landing_page/Contact.js
@@ -39,7 +39,7 @@ function Contact() {
          
         <div class="col-xl-3 col-sm-6 mb-5">
             <div class="bg-white rounded shadow-sm py-5 px-4"><img src="" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
-                <h5 class="mb-0">Prasaddhi Dahal</h5>
+                <h5 class="mb-0">Prasiddhi Dahal</h5>
                 <ul class="social mb-0 list-inline mt-3">
                     <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
                     <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
