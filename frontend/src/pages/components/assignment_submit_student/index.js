d0862456d82b1a0a3e2281071407fc32705e2fce
Azhe0 <82790572+Azhe0@users.noreply.github.com>
2023-04-18 16:18:59 +0545

    ggs push and pull

---
+++ frontend/src/pages/components/assignment_submit_student/index.js
@@ -0,0 +1,40 @@
+import x from '/src/styles/Navbar.module.css';
+import Link from 'next/link';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import as from '/src/styles/AssignmentSubmit.module.css';
+import { useState } from 'react'
+
+export default function Assignment() {
+  const [file, setFile] = useState(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log('Only PUT PDF OR DOCX FILES')
+  }
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  return (
+    <div>
+      <h1>Assignment Submission</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="file">Select a file:</label>
+          <input
+            type="file"
+            id="file"
+            class={as.file_upload}   
+            accept=".pdf,.doc,.docx,.txt"
+            onChange={handleFileChange}
+            placeholder="Select a file"
+            
+          />
+        </div>
+        <button type="submit">POST NOW</button>
+      </form>
+    </div>
+  )
+}
--- frontend/src/pages/components/landing_page/Contact.js
+++ frontend/src/pages/components/landing_page/Contact.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import Image from 'next/image';
+import person_image from '/public/person.png';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -19,7 +21,7 @@ function Contact() {
 
   
             <div class="col-xl-3 col-sm-6 mb-5">
-                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="/person.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <div class="bg-white rounded shadow-sm py-5 px-4"><Image src={person_image} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                     <h5 class="mb-0">Sarthak Shakya</h5>
                     <ul class="social mb-0 list-inline mt-3">
                         <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
@@ -28,7 +30,7 @@ function Contact() {
                 </div>
             </div>
             <div class="col-xl-3 col-sm-6 mb-5">
-                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-1.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <div class="bg-white rounded shadow-sm py-5 px-4"><Image src={person_image} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                     <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                     <ul class="social mb-0 list-inline mt-3">
                         <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
@@ -37,7 +39,7 @@ function Contact() {
                 </div>
             </div>
             <div class="col-xl-3 col-sm-6 mb-5">
-                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-1.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <div class="bg-white rounded shadow-sm py-5 px-4"><Image src={person_image} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                     <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                     <ul class="social mb-0 list-inline mt-3">
                         <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
@@ -48,7 +50,7 @@ function Contact() {
 
       
             <div class="col-xl-3 col-sm-6 mb-5">
-                <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-7.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <div class="bg-white rounded shadow-sm py-5 px-4"><Image src={person_image} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                     <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                     <ul class="social mb-0 list-inline mt-3">
                         <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
