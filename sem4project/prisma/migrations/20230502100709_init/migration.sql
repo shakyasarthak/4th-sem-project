a02e70cd219f62c4179af8e7db201cafb0182562
AKIRAFOX <122764949+akira-lawliet@users.noreply.github.com>
2023-05-02 15:53:01 +0545

    more db

--- sem4project/prisma/migrations/20230502100709_init/migration.sql
+++ sem4project/prisma/migrations/20230502100709_init/migration.sql
@@ -18,8 +18,34 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "student" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "assignment" DECIMAL(65,30) NOT NULL,
+    "quiz" DECIMAL(65,30) NOT NULL,
+
+    CONSTRAINT "student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "teacher" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "teacher_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "student_email_key" ON "student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teacher_email_key" ON "teacher"("email");
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
