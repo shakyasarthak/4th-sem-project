a75a1dad836e6d1ab908f95f575ff6e4edb63f0e
AKIRAFOX <122764949+akira-lawliet@users.noreply.github.com>
2023-05-03 09:22:12 +0545

    update package

--- sem4project/.next/static/chunks/webpack.js
+++ sem4project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "783493dd76959555"; }
+/******/ 		__webpack_require__.h = function() { return "aa4b13fd9a08017a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
